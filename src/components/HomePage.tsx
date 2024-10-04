4edef4b1c83393de84803fbb33105d2cdf924cda
Sosobo-code <arrshan14@gmail.com>
2024-10-04 21:49:20 +1300

    Good?

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -5,8 +5,8 @@ const HomePage = () => {
     return (
         <div className=" justify-center items-center h-screen" >
             <div className="flex flex-col justify-center items-center h-screen">
-                <h1 className="font-bold text-3xl">Lorem Ispum Dolor Sit Amet</h1>
-                <Button>Get Started</Button>
+                <h1 className="font-semibold text-5xl mb-8">Lorem Ispum Dolor Sit Amet</h1>
+                <Button className="w-1/6">Get Started</Button>
             </div>
 
         </div>
