543ecbee273323a76957e00252737b229e2d86fd
Michael Bui <michael.bui_crystal@outlook.com>
2024-10-05 00:24:36 +1300

    refactor nav bar code

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,9 +13,10 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 import React, {useEffect, useState} from "react";
-import {cn} from "@/lib/utils";
 import {useTheme} from "next-themes";
 import {ThemeToggle} from "@/components/ui/ThemeToggle";
+import NavBarMobile from "@/components/NavBarMobile";
+import {cn} from "@/lib/utils";
 
 export default function NavBar() {
   const {theme} = useTheme();
@@ -117,67 +118,7 @@ export default function NavBar() {
           </button>
         </div>
       </div>
-      {isMobileMenuOpen && (
-        <div className="md:hidden flex flex-col items-center text-center pull-down">
-          <NavigationMenu className="pb-2">
-            <NavigationMenuList>
-              <NavigationMenuItem>
-                <Link href="/" passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    <Home className="mr-2 h-4 w-4"/>
-                    Home
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>
-                  <ShoppingBag className="mr-2 h-4 w-4"/>
-                  Store
-                </NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <ul className="grid gap-3 p-6">
-                    <li className="row-span-3">
-                      <NavigationMenuLink asChild>
-                        <Link
-                          className={`flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md ${theme === 'dark' ? 'animate-gradient' : 'light-mode-animate-gradient'}`}
-                          href="/"
-                        >
-                          <div className="mb-2 mt-4 text-lg font-medium">
-                            Wrap Colours
-                          </div>
-                          <p className="text-sm leading-tight text-muted-foreground">
-                            View the catalogue of wrap colours.
-                          </p>
-                        </Link>
-                      </NavigationMenuLink>
-                    </li>
-                    <ListItem href="/" title="Shop">
-                      View the store.
-                    </ListItem>
-                    <ListItem href="/" title="Accessories">
-                      View the accessories.
-                    </ListItem>
-                    <ListItem href="/services" title="Services">
-                      Explore our range of services.
-                    </ListItem>
-                  </ul>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <Link href="/" passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    <Palette className="mr-2 h-4 w-4"/>
-                    Colour Picker
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
-          <Button asChild variant="outline" className="w-1/2 mb-2">
-            <Link href="/">Contact Us</Link>
-          </Button>
-        </div>
-      )}
+      <NavBarMobile isMobileMenuOpen={isMobileMenuOpen} />
       <div className="fixed bottom-4 right-4">
         <ThemeToggle/>
       </div>
@@ -185,7 +126,7 @@ export default function NavBar() {
   )
 }
 
-const ListItem = React.forwardRef<
+export const ListItem = React.forwardRef<
   React.ElementRef<"a">,
   React.ComponentPropsWithoutRef<"a">
 >(({className, title, children, ...props}, ref) => {
