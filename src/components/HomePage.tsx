d9929237758f223aed730000dc55bd4346e16734
Sosobo-code <arrshan14@gmail.com>
2024-10-04 21:33:20 +1300

    Added Homepage Placeholder

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,8 +3,12 @@ import {Button} from "@/components/ui/button";
 
 const HomePage = () => {
     return (
-        <div className="flex justify-center items-center h-screen">
-            <h1 className="font-bold text-3xl">Lorem Ispum Dolor Sit Amet</h1>
+        <div className=" justify-center items-center h-screen" >
+            <div className="flex flex-col justify-center items-center h-screen">
+                <h1 className="font-bold text-3xl">Lorem Ispum Dolor Sit Amet</h1>
+                <Button>Get Started</Button>
+            </div>
+
         </div>
     );
 };
