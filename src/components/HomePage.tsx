1ca464c2163e8e7b4f4661e119954b79acc71583
Michael Bui <michael.bui_crystal@outlook.com>
2024-10-06 18:50:49 +1300

    formatting

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,25 +2,25 @@ import React from 'react';
 import {Button} from "@/components/ui/button";
 
 const HomePage = () => {
-    return (
-        <div className="relative flex justify-center items-center h-screen overflow-hidden">
-            <video
-                autoPlay
-                loop
-                muted
-                className="absolute top-0 left-0 w-full h-full object-cover"
-            >
-                <source src="/assets/video.mp4" type="video/mp4"/>
-                Your browser does not support the video tag.
-            </video>
-            <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"/>
-            <div className="relative flex flex-col justify-center items-center h-screen z-10 px-4 text-center">
-                <h1 className="font-semibold text-4xl md:text-5xl mb-4 md:mb-8 text-white">Lorem Ipsum Dolor Sit Amet</h1>
-                <Button className="w-full md:w-1/2 sm:w-1/8">Get Started</Button>
-            </div>
-        </div>
+  return (
+    <div className="relative flex justify-center items-center h-screen overflow-hidden">
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute top-0 left-0 w-full h-full object-cover"
+      >
+        <source src="/assets/video.mp4" type="video/mp4"/>
+        Your browser does not support the video tag.
+      </video>
+      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"/>
+      <div className="relative flex flex-col justify-center items-center h-screen z-10 px-4 text-center">
+        <h1 className="font-semibold text-4xl md:text-5xl mb-4 md:mb-8 text-white">Lorem Ipsum Dolor Sit Amet</h1>
+        <Button className="w-full md:w-1/2 sm:w-1/8">Get Started</Button>
+      </div>
+    </div>
 
-    );
+  );
 };
 
 export default HomePage;
