36a0b4aafbd94e788e05f98ecc01e9dcc935c512
Sosobo-code <arrshan14@gmail.com>
2024-10-15 09:16:24 +1300

    Made footer work with shadcn light and dark modes.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="text-center p-3 bg-background text-white">
+    <div className="text-center p-3 bg-background text-lg">
       <p>AlphaDrift Wraps</p>
       <a href="example@mail.com">Example@mail.com</a>
     </div>
